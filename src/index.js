b1c4f7a446e3c41c88099069b2b960f687798bcc
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-21 13:52:39 -0600

    added boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// External imports
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
+// Local imports
 import App from "./App";
+import rootReducer from "./reducers";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
+// Assets
+import "./style.css";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </Provider>,
+  document.getElementById("root")
 );
