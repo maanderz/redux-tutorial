796c9c858b7eb0ab625d6b1916ebcd2e6f8df397
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-22 13:40:30 -0600

    connected react with redux

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
+// Bring in the asynchronous fetchPosts action
+import { fetchPosts } from "../actions/postsAction";
+import { Post } from "../components/Post";
+
 // Redux state is now in the props of the component
-const PostsPage = ({ loading, posts, hasErrors }) => {
+const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  // Show loading, error, or success state
+  const renderPosts = () => {
+    if (loading) return <p>Loading posts...</p>;
+    if (hasErrors) return <p>Unable to display posts.</p>;
+    return posts.map(post => <Post key={post.id} post={post} />);
+  };
+
   return (
     <section>
       <h1>Posts</h1>
+      {renderPosts()}
     </section>
   );
 };
@@ -14,7 +30,7 @@ const PostsPage = ({ loading, posts, hasErrors }) => {
 const mapStateToProps = state => ({
   loading: state.posts.loading,
   posts: state.posts.posts,
-  hasErrors: state.props.hasErrors
+  hasErrors: state.posts.hasErrors
 });
 
 // Connect Redux to React
